0c824889ce4cafdd7b3ab17befd82f3f0d4bbc61
Your Name <tfikrie@yahoo.com>
2019-08-30 19:45:25 -0400

    index.js, server and router  setup, knexindex.js configured to incude mogationa nd seed directory

---
+++ project-router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const db = require('./Data/project-model');
+
+const router = express.Router();
+
+router.get('/',(req, res)=>{
+    
+})
+
+module.exports = router;
